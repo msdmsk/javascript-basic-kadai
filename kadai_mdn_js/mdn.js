cd2f8700db2240b7987a7c450b72c1956ab4b2bb
msdmsk <msdmsk0224@gmail.com>
2023-11-08 10:13:39 +0900

    mdn

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,8 +1,8 @@
-const now = Date();
-console.log(now);
+const now = new Date();
+// console.log(now);
 
 const year = now.getFullYear();
-const month = now.getMonth();
+const month = now.getMonth() + 1;
 const day = now.getDate();
 
 console.log(`${year}年${month}月${day}日`);
